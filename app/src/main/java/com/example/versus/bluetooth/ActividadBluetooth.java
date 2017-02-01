084b0d6e5b9a348dd6fad5c239a572f500e97e1c
SergoRz <sanchez13rz@gmail.com>
2017-02-01 20:20:47 +0100

    VAMONOH!

--- app/src/main/java/com/example/versus/bluetooth/ActividadBluetooth.java
+++ app/src/main/java/com/example/versus/bluetooth/ActividadBluetooth.java
@@ -27,17 +27,17 @@ import java.util.Set;
  * Esta clase es la Activity principal del programa, nos mostrará la interfaz grafica
  * y nos permitira acceder a todas las funcionalidades de la aplicacion.
  */
-public class ActividadBluetooth extends Activity implements CheckBox.OnCheckedChangeListener,ListView.OnItemClickListener{
-    private int estado=Constantes.SIN_CONECTAR;
+public class ActividadBluetooth extends Activity implements CheckBox.OnCheckedChangeListener,ListView.OnItemClickListener {
+    private int estado = Constantes.SIN_CONECTAR;
     private final static int HABILITA_BT = 1;
     private BluetoothAdapter btAdapter;
-    private boolean mActivado=false; //por defecto no está activado
+    private boolean mActivado = false; //por defecto no está activado
     private ArrayAdapter<String> arrayDispositivos;
     private ListView lista_dispositivos;
-    private int seleccionado=-1;
+    private int seleccionado = -1;
     private BluetoothDevice dispositivoConectado;
     private BluetoothSocket socket;
-    private TextView txtEstado,txtEnviar,txtRecibir;
+    private TextView txtEstado, txtEnviar, txtRecibir;
     private CheckBox checkbox;
     private AcceptThread mAcceptThread;
     private ConnectThread mConnectThread;
@@ -46,20 +46,21 @@ public class ActividadBluetooth extends Activity implements CheckBox.OnCheckedCh
     /**
      * Metodo que se ejecuta al iniciar la Activity.
      * Carga la interfaz de usuario y registra los dispositivos encontrados
+     *
      * @param savedInstanceState
      */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        checkbox = (CheckBox)findViewById(R.id.cbVinculadas);
+        checkbox = (CheckBox) findViewById(R.id.cbVinculadas);
         checkbox.setOnCheckedChangeListener(this);
-        lista_dispositivos = (ListView)findViewById(R.id.lvDispositivos);
+        lista_dispositivos = (ListView) findViewById(R.id.lvDispositivos);
         lista_dispositivos.setOnItemClickListener(this);
-        arrayDispositivos = new ArrayAdapter<>(this,android.R.layout.simple_list_item_1);
-        txtEstado = (TextView)findViewById(R.id.tvEstado);
-        txtEnviar = (TextView)findViewById(R.id.txtEnviar);
-        txtRecibir = (TextView)findViewById(R.id.txtRecibido);
+        arrayDispositivos = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1);
+        txtEstado = (TextView) findViewById(R.id.tvEstado);
+        txtEnviar = (TextView) findViewById(R.id.txtEnviar);
+        txtRecibir = (TextView) findViewById(R.id.txtRecibido);
         estado = Constantes.SIN_CONECTAR;
 
         //Registra el receptor de descubrir dispositivos
@@ -71,26 +72,25 @@ public class ActividadBluetooth extends Activity implements CheckBox.OnCheckedCh
     /**
      * Método que comprueba si el dispositivo soporta Bluetooth, en ese caso comprueba si esta
      * activado, si no lo esta solicita al usuario que lo active.
+     *
      * @param v View que produce este evento
      */
-    public void IniciarBluetooth(View v){
+    public void IniciarBluetooth(View v) {
         //Obtiene el adaptador por defecto, si obtiene null significa que no tiene,
         //con lo cual el dispositivo no adminte bluetooth
         btAdapter = BluetoothAdapter.getDefaultAdapter();
-        if (btAdapter== null) {
+        if (btAdapter == null) {
             // El dispositivo no soporta Bluetooth
-            Toast.makeText(this,"El dispositivo no soporta bluetooth",Toast.LENGTH_LONG).show();
-        }
-        else{
+            Toast.makeText(this, "El dispositivo no soporta bluetooth", Toast.LENGTH_LONG).show();
+        } else {
             if (!btAdapter.isEnabled()) {
                 // El dispositivo soporta Bluetooth, pero no está activado
                 // Se solicita al usuario que conecte el Bluetooth
                 Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
                 startActivityForResult(enableBtIntent, HABILITA_BT);
-            }
-            else{
+            } else {
                 //En este caso el dispositivo soporta Bluetooth y esta activado
-                mActivado=true;
+                mActivado = true;
             }
         }
 
@@ -100,18 +100,19 @@ public class ActividadBluetooth extends Activity implements CheckBox.OnCheckedCh
 
     /**
      * Método que obtiene la respuesta del usuario cuando se le solicita activar el Bluetooth.
+     *
      * @param requestCode Codigo de la solicitud
-     * @param resultCode Codigo de la respuesta
-     * @param data Intent que se lanza para recibir dicha respuesta
+     * @param resultCode  Codigo de la respuesta
+     * @param data        Intent que se lanza para recibir dicha respuesta
      */
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if(requestCode==HABILITA_BT)
-            if(resultCode==RESULT_OK) {
+        if (requestCode == HABILITA_BT)
+            if (resultCode == RESULT_OK) {
                 //Se activo el Bluetooth
                 Toast.makeText(this, "El dispositivo activo el bluetooth", Toast.LENGTH_LONG).show();
                 mActivado = true;
-            }else{
+            } else {
                 //No se activo el Bluetooth
                 Toast.makeText(this, "No se ha activado el bluetooth", Toast.LENGTH_LONG).show();
             }
@@ -119,22 +120,21 @@ public class ActividadBluetooth extends Activity implements CheckBox.OnCheckedCh
 
     @Override
     public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
-        if(btAdapter != null)
-            if(b) {
+        if (btAdapter != null)
+            if (b) {
                 arrayDispositivos.clear();
                 Descubre();
-            }
-            else{
+            } else {
                 VerDispositivos();
             }
     }
 
-    public void VerDispositivos(){
+    public void VerDispositivos() {
         EncuentraEmparejados();
         lista_dispositivos.setAdapter(arrayDispositivos);
     }
 
-    public void EncuentraEmparejados(){
+    public void EncuentraEmparejados() {
         Set<BluetoothDevice> pairedDevices = btAdapter.getBondedDevices();
         // Si hay dispositivos emparejados
         if (pairedDevices.size() > 0) {
@@ -145,7 +145,7 @@ public class ActividadBluetooth extends Activity implements CheckBox.OnCheckedCh
         }
     }
 
-    public void Descubre(){
+    public void Descubre() {
         if (btAdapter.isDiscovering()) {
             btAdapter.cancelDiscovery();
         }
@@ -154,31 +154,31 @@ public class ActividadBluetooth extends Activity implements CheckBox.OnCheckedCh
 
     public synchronized void Conectar(BluetoothSocket socket, BluetoothDevice device) {
         // Comienza la conexión!!
-        mConnectedThread = new ConnectedThread(socket,mHandler);
+        mConnectedThread = new ConnectedThread(socket, mHandler);
         mConnectedThread.start();
     }
 
-    public void Enviar(View v){
+    public void Enviar(View v) {
         enviarMensaje(txtEnviar.getText().toString());
     }
 
-    public void IniciarServidor(View v){
-        if(btAdapter!=null) {
+    public void IniciarServidor(View v) {
+        if (btAdapter != null) {
             mAcceptThread = new AcceptThread(btAdapter, mHandler, this);
             mAcceptThread.start();
-        }
-        else{
-            Toast.makeText(this,"Por favor, dale al boton de iniciar",Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, "Por favor, dale al boton de iniciar", Toast.LENGTH_SHORT).show();
         }
     }
 
     /**
      * Metodo que se encarga de iniciar un dispositivo como Cliente
+     *
      * @param v Boton Iniciar cliente
      */
-    public void IniciarCliente(View v){
+    public void IniciarCliente(View v) {
         //Si no se ha escogido un dispositivo al cual conectarse..
-        if(seleccionado==-1)
+        if (seleccionado == -1)
             Toast.makeText(this, "elige un dispositivo al que conectarte primero", Toast.LENGTH_LONG).show();
         else { //Si se ha escogido..
             //Se recoge el nombre del dispositivo
@@ -187,12 +187,11 @@ public class ActividadBluetooth extends Activity implements CheckBox.OnCheckedCh
             String address = x.substring(x.length() - 17);
             //Se establece la conexion con el dispositivo
             dispositivoConectado = btAdapter.getRemoteDevice(address);
-            if(dispositivoConectado != null) { //Si se ha establecido la conexion correctamente..
+            if (dispositivoConectado != null) { //Si se ha establecido la conexion correctamente..
                 //Se lanza el hilo ConnectThread
                 mConnectThread = new ConnectThread(dispositivoConectado, btAdapter, mHandler, this);
                 mConnectThread.start();
-            }
-            else
+            } else
                 Toast.makeText(this, "no pude obtener enlace al dispositivo", Toast.LENGTH_LONG).show();
         }
     }
@@ -209,10 +208,11 @@ public class ActividadBluetooth extends Activity implements CheckBox.OnCheckedCh
 
     /**
      * Metodo que se encarga de guardar un dispositivo cuando ha sido clicado
+     *
      * @param adapterView Adaptador de la lista de dispositivos
-     * @param view Vista de la ListView
-     * @param i Item seleccionado
-     * @param l Longitud
+     * @param view        Vista de la ListView
+     * @param i           Item seleccionado
+     * @param l           Longitud
      */
     @Override
     public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
@@ -221,10 +221,11 @@ public class ActividadBluetooth extends Activity implements CheckBox.OnCheckedCh
 
     /**
      * Metodo que se encarga de enviar un mensaje escribiendolo en el OutputStream del socket
+     *
      * @param mensaje Mensaje que se desea enviar.
      */
     private void enviarMensaje(String mensaje) {
-        if (estado==Constantes.SIN_CONECTAR) { //Si no esta conectado..
+        if (estado == Constantes.SIN_CONECTAR) { //Si no esta conectado..
             Toast.makeText(this, "conecta primero a un servidor!", Toast.LENGTH_SHORT).show();
             return;
         }
@@ -236,48 +237,19 @@ public class ActividadBluetooth extends Activity implements CheckBox.OnCheckedCh
             mConnectedThread.write(send);
         }
     }
-
-    /**
-     * Handler que recibe la información de los Threads
-     * Actualiza el txtEstado de la interfaz informando al usuario del estado de la aplicacion,
-     * dependiendo del mensaje que recibe de los Threads
-     */
-    private final Handler mHandler = new Handler() {
-        @Override
-        public void handleMessage(Message msg) {
-            switch (msg.what) {
-                case Constantes.CAMBIAR_ESTADO:
-                    if(msg.arg1==Constantes.ESTADO_CONECTADO)
-                        CambiarEstado(msg.arg1, msg.getData().getString(Constantes.NOMBRE_DISPOSITIVO));
-                    else
-                        CambiarEstado(msg.arg1, "");
-                    break;
-                case Constantes.MENSAJE_ENVIADO:
-                    CambiarEstado(Constantes.MENSAJE_ENVIADO,"");
-                    break;
-                case Constantes.MENSAJE_RECIBIDO:
-                    byte[] readBuf = (byte[]) msg.obj;
-                    // construye una cadena de caracteres a partir de los caracteres del buffer
-                    String readMessage = new String(readBuf, 0, msg.arg1);
-                    txtRecibir.setText(readMessage);
-                    CambiarEstado(Constantes.MENSAJE_RECIBIDO,"");
-                    break;
-            }
-        }
-    };
-
     /**
      * Metodo que se encarga de actualizar la interfaz de la aplicacion para poder visualizar en
      * que estado se encuentra la aplicacion
-     * @param i Mensaje de la calse Constantes que se quiere notificar
+     *
+     * @param i     Mensaje de la calse Constantes que se quiere notificar
      * @param extra Informacion extra que se quiere añadir, se utiliza para indicar el nombre del
      *              dispositivo con el que se ha establecido la conexion
      */
-    public void CambiarEstado(int i, String extra){
-        estado=i;
-        switch(i){
+    public void CambiarEstado(int i, String extra) {
+        estado = i;
+        switch (i) {
             case Constantes.ESTADO_CONECTADO:
-                txtEstado.setText("Conectado: "+extra);
+                txtEstado.setText("Conectado: " + extra);
                 break;
             case Constantes.ESTADO_CONECTANDO:
                 txtEstado.setText("Conectando");
@@ -292,8 +264,42 @@ public class ActividadBluetooth extends Activity implements CheckBox.OnCheckedCh
                 txtEstado.setText("Mensaje enviado");
                 break;
         }
+    }
+
+
+    //Clases Anónimas
+
+    /**
+     * Handler que recibe la información de los Threads
+     * Actualiza el txtEstado de la interfaz informando al usuario del estado de la aplicacion,
+     * dependiendo del mensaje que recibe de los Threads
+     */
+    private final Handler mHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case Constantes.CAMBIAR_ESTADO:
+                    if (msg.arg1 == Constantes.ESTADO_CONECTADO)
+                        CambiarEstado(msg.arg1, msg.getData().getString(Constantes.NOMBRE_DISPOSITIVO));
+                    else
+                        CambiarEstado(msg.arg1, "");
+                    break;
+                case Constantes.MENSAJE_ENVIADO:
+                    CambiarEstado(Constantes.MENSAJE_ENVIADO, "");
+                    break;
+                case Constantes.MENSAJE_RECIBIDO:
+                    byte[] readBuf = (byte[]) msg.obj;
+                    // construye una cadena de caracteres a partir de los caracteres del buffer
+                    String readMessage = new String(readBuf, 0, msg.arg1);
+                    txtRecibir.setText(readMessage);
+                    CambiarEstado(Constantes.MENSAJE_RECIBIDO, "");
+                    break;
+            }
+        }
+    };
+
     // Crea un BroadcastReceiver para ACTION_FOUND
-    private final BroadcastReceiver mReceiver = new BroadcastReceiver() {
+    public final BroadcastReceiver mReceiver = new BroadcastReceiver() {
         public void onReceive(Context context, Intent intent) {
             String action = intent.getAction();
             // Cuando se descubre un dispositivo
